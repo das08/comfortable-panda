7348b7e60293fbeb454577c364cd09cf661413fc
tinaxd <31384595+tinaxd@users.noreply.github.com>
2020-10-06 17:17:19 +0900

    onGetAllLectureInfoRequest onGetLastKadaiUpdateTime

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 function onGetAllKadaiRequest(msg) {
-    if (msg.type === 'get-all-kadai') {
+    if (msg.type === 'raw-get-all-kadai') {
         return new Promise((resolve, reject) => {
             chrome.storage.local.get('kadai', (item) => {
                 resolve(item);
@@ -8,4 +8,26 @@ function onGetAllKadaiRequest(msg) {
     }
 }
 
-browser.runtime.onMessageExternal.addListener(onGetAllKadaiRequest);
+function onGetAllLectureInfoRequest(msg) {
+    if (msg.type === 'raw-get-all-lecture-info') {
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.get('lectureInfo', (item) => {
+                resolve(item);
+            });
+        });
+    }
+}
+
+function onGetLastKadaiUpdateTime(msg) {
+    if (msg.type === 'raw-get-last-kadai-update-time') {
+        return new Promise((resolve, reject) => {
+            chrome.storage.local.get('lastKadaiGetTime', (item) => {
+                resolve(item);
+            });
+        });
+    }
+}
+
+chrome.runtime.onMessageExternal.addListener(onGetAllKadaiRequest);
+chrome.runtime.onMessageExternal.addListener(onGetAllLectureInfoRequest);
+chrome.runtime.onMessageExternal.addListener(onGetLastKadaiUpdateTime);
